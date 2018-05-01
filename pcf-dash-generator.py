ec3f7a9f2c48818a8037b0f1b261ea5a29006756
Jeff Holmes <jeff.holmes@osxltjholm.domain>
2018-05-01 12:47:16 -0700

    initial commit

---
+++ pcf-dash-generator.py
@@ -0,0 +1,105 @@
+
+#!flask/bin/python
+from flask import Flask, jsonify
+from string import Template
+import argparse
+import requests
+
+pcf_dash_template_file = 'templates/pcf_dashboard_template_v1.json'
+pcf_dash_generated_file = 'generated/pcf_dashboard_generated.json'
+pcf_hrs_template_file = 'templates/pcf_healthrules_template_v1.json'
+pcf_hrs_generated_file = 'generated/pcf_healthrules_generated.json'
+
+app = Flask(__name__)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-controller_url", help='the controller url, i.e. http://mycontroller:8090', required=True)
+    parser.add_argument("-user_name", help='the controller username (user@account)', required=True)    
+    parser.add_argument("-user_pass", help='the controller user password', required=True)        
+    parser.add_argument("-app", help='the Appd app where PCF metrics are published by the PCF tile', required=True)            
+    parser.add_argument("-tier", help='the Appd tier where PCF metrics are published by the PCF tile', required=True)                
+    parser.add_argument("-v", "--verbose", action='store_true', dest="verbose", default=False, help="print verbose output")
+    args = parser.parse_args()
+    if args.verbose:
+        print(args)
+    return args
+    
+def populate_service_details(args):
+    metric_path_root = 'Application Infrastructure Performance|' + args.tier + '|Custom Metrics|CF|cf'    
+    query_prams='?output=json&metric-path=' + metric_path_root
+    url = args.controller_url + '/controller/rest/applications/' + args.app + '/metrics' + query_prams
+    if args.verbose: print('url: ' + url)    
+    response = requests.get(url, auth=(args.user_name, args.user_pass))
+    response.raise_for_status();
+    print(response.json()) 
+    pcf_service_list = response.json()
+
+    pcf_services = {}
+    for pcf_service in pcf_service_list:
+        service_name = pcf_service['name']
+        print('service: ' + service_name)
+        pcf_service_metric_path = metric_path_root + '|' + service_name 
+        query_prams='?output=json&metric-path=' + pcf_service_metric_path
+        service_url = args.controller_url + '/controller/rest/applications/' + args.app + '/metrics' + query_prams
+        print ('service_url: ' + service_url)
+        response = requests.get(service_url, auth=(args.user_name, args.user_pass))
+        response.raise_for_status();
+        service_instances = response.json()
+        print('nbr of instances: ' + str(len(service_instances))) 
+        pcf_services[service_name] = [dict() for x in range(len(service_instances))]
+        for i, service_instance in enumerate(service_instances):
+            print('service instance: ' + str(service_instance))
+            guid = service_instance['name']
+            pcf_services[service_name][i] = {'guid' : guid, 'ips' : []}
+            guid_url = service_url + '|' + guid
+            print ('guid_url: ' + guid_url)
+            response = requests.get(guid_url, auth=(args.user_name, args.user_pass))
+            response.raise_for_status();
+            ips = response.json()
+            print('ips: ' + str(ips))
+            pcf_services[service_name][i]['ips'] = [None] * len(ips)
+            for j, ip in enumerate(ips):
+                 #pcf_services[service_name][i] = {'guid' : guid, 'ips' : []}
+                 print('ip: ' + ip['name'])
+                 pcf_services[service_name][i]['ips'][j] = ip['name']
+
+    return pcf_services
+
+def generate_dashboard(pcf_services, app, tier):
+    with open(pcf_dash_template_file, 'r', encoding='utf-8') as myfile:
+        dash_template=myfile.read()
+    dash_template = Template(dash_template)
+    generated = dash_template.substitute(APP_NAME=app,
+                                 TIER_NAME=tier, 
+                                 DIEGO_CELL_0_GUID=pcf_services['diego_cell'][0]['guid'], 
+                                 DIEGO_CELL_0_IP_0=pcf_services['diego_cell'][0]['ips'][0],
+                                 DIEGO_CELL_1_GUID=pcf_services['diego_cell'][1]['guid'], 
+                                 DIEGO_CELL_1_IP_0=pcf_services['diego_cell'][1]['ips'][0],
+                                 DIEGO_CELL_2_GUID=pcf_services['diego_cell'][2]['guid'], 
+                                 DIEGO_CELL_2_IP_0=pcf_services['diego_cell'][2]['ips'][0])
+    return generated
+
+    
+def start_flask():
+    app.run(debug=True, port=8082)
+
+@app.route('/publish')
+def publish():
+    gen()
+    return 'done!'
+    
+def run():
+    args = parse_args()
+    pcf_services = populate_service_details(args)
+    print('pcf_services: ' + str(pcf_services))
+    generated = generate_dashboard(pcf_services, args.app, args.tier)
+    with open(pcf_dash_generated_file, 'w', encoding='utf-8') as myfile:
+        myfile.write(generated)
+    
+if __name__ == '__main__':
+    run()
+
+
+
+    #start_flask()
